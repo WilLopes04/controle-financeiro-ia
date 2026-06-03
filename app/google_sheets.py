e40fc8cfc0229b1f7a3deb410679885458d26370
WilLopes04 <wilsonfilho2004@hotmail.com>
2026-06-03 13:07:52 -0300

    Render usando credenciaias por variavel de ambiente

--- app/google_sheets.py
+++ app/google_sheets.py
@@ -1,11 +1,14 @@
 import gspread
+import json
 from google.oauth2.service_account import Credentials
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-
+google_creds = json.loads(
+    os.environ[GOOGLE_CREDENTIALS_JSON] 
+)
 creds = Credentials.from_service_account_file(
     "credentials.json",
     scopes=SCOPES
